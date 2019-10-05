4ebe6e5b9951ed1a99ad906a2bcb68d80ce88bad
sapirely <Sapsap11>
2019-10-05 19:16:47 +0300

    FAB setup

--- app/src/main/java/postpc/studypartner2/HomeFragment.java
+++ app/src/main/java/postpc/studypartner2/HomeFragment.java
@@ -1,23 +1,21 @@
 package postpc.studypartner2;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
-import androidx.navigation.NavController;
-import androidx.navigation.NavDirections;
 import androidx.navigation.Navigation;
-import androidx.navigation.ui.NavigationUI;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 
 public class HomeFragment extends Fragment {
+
+    private static final String TAG = "HomeFragment";
+    private FloatingActionButton floatingActionButton;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -28,12 +26,23 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_home, container, false);
-//
-//        NavController navController = Navigation.findNavController(requireActivity(), R.id.nav_host_fragment);
-////        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
-//        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+
+        // Set up UI
+        setUpFAB(view);
 
         return view;
+    }
+
+    private void setUpFAB(View view){
+        floatingActionButton = view.findViewById(R.id.floatingActionButton);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(getActivity(), R.id.nav_host_fragment)
+                        .navigate(R.id.action_homeFragment_to_searchFragment);
+
+            }
+        });
     }
 
 }
